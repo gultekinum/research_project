476833571954f3d9913b572c26905c0a6fc11c39
unknown <umutgultekingsu@gmail.com>
2021-03-12 01:07:45 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import hashlib
+from block import Block
+import random
+
+block_size = 10
+blockchain = []
+degree=3
+
+f = open("vote_list.txt","r")
+vote_data = f.read().split("\n")
+
+def validate_hash(hash_code):
+    global block_size,degree
+    rule = "0"*degree
+    nonce=0
+    temp_string = hash_code+str(nonce)
+    new_hash = hashlib.sha256(temp_string.encode()).hexdigest()
+    while new_hash[0:degree]!=rule:
+        nonce+=1
+        temp_string = hash_code+str(nonce)
+        new_hash = hashlib.sha256(temp_string.encode()).hexdigest()
+    return str(nonce)
+
+
+genesis_block = Block("genesis",["genesis block"],"0000000")
+blockchain.append(genesis_block)
+
+block_count = 0
+
+for i in range(0,len(vote_data)-block_size+1,block_size):
+    current_hash = blockchain[len(blockchain)-1].block_hash+"".join(vote_data[i:i+block_size])
+    validation_nonce = validate_hash(current_hash)
+    block = Block(blockchain[len(blockchain)-1].block_hash,vote_data[i:i+block_size],validation_nonce)
+    blockchain.append(block)
+    block_count+=1
+
+for block in blockchain:
+    block.get_block_data()
+
+
---
+++ prepare.py
@@ -0,0 +1,27 @@
+import random
+from random import randint
+import sys
+
+party_list =["A Party","B Party","C Party","D Party"]
+
+def prepare_vote_data(person_count):
+    
+    person_list = []
+    f = open("person_list.txt","w")
+    for i in range(person_count):
+        num = randint(10000000000,99999999999)
+        party = random.choice(party_list)
+        while num in person_list:
+            num = random.randint(10000000000,99999999999)
+            party = random.choice(party_list)
+        person_list.append("{} voted for {}".format(str(num),party))
+    f = open("vote_list.txt","w")
+    f.write("\n".join(person_list))
+
+
+prepare_vote_data(int(sys.argv[1]))
+
+
+
+
+    
